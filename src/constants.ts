1d6d047ecacab7ee52e9fab6467bec0f33b0602b
Lucas Flaquer <lucas.flaquer@gmail.com>
2024-03-07 20:51:01 -0300

    index POkemon component

---
+++ src/constants.ts
@@ -0,0 +1,92 @@
+interface Constants {
+  // colors: {
+  //   [key: string]: string
+  // }
+  pokemonTypes: {
+    [key: string]: {
+      background: string
+      color: string
+    }
+  }
+}
+
+export const constants: Constants = {
+  // colors: {
+  //   aaa: ''
+  // },
+  pokemonTypes: {
+    bug: {
+      color: '#8CB230',
+      background: '#8BD674'
+    },
+    dark: {
+      color: '#58575F',
+      background: '#6F6E78'
+    },
+    dragon: {
+      color: '#0F6AC0',
+      background: '#7383B9'
+    },
+    eletric: {
+      color: '#EED535',
+      background: '#F2CB55'
+    },
+    fairy: {
+      color: '#ED6EC7',
+      background: '#EBA8C3'
+    },
+    fighting: {
+      color: '#D04164',
+      background: '#EB4971'
+    },
+    fire: {
+      color: '#FD7D24',
+      background: '#FFA756'
+    },
+    flying: {
+      color: '#748FC9',
+      background: '#83A2E3'
+    },
+    ghost: {
+      color: '#556AAE',
+      background: '#8571BE'
+    },
+    grass: {
+      color: '#62B957',
+      background: '#8BBE8A'
+    },
+    ground: {
+      color: '#DD7748',
+      background: '#F78551'
+    },
+    ice: {
+      color: '#61CEC0',
+      background: '#91D8DF'
+    },
+    normal: {
+      color: '#9DA0AA',
+      background: '#B5B9C4'
+
+    },
+    poison: {
+      color: '#A552CC',
+      background: '#9F6E97'
+    },
+    psychic: {
+      color: '#EA5D60',
+      background: '#FF6568'
+    },
+    rock: {
+      color: '#BAAB82',
+      background: '#D4C294'
+    },
+    steel: {
+      color: '#417D9A',
+      background: '#4C91B2'
+    },
+    water: {
+      color:'#4A90DA',
+      background: '#58ABF6'
+    }
+  }
+}
---
+++ src/screens/Home/Pokemon/index.tsx
@@ -0,0 +1,80 @@
+import { Image, StyleSheet, Text, View } from 'react-native';
+import { Pokemon } from '..';
+import { TypeIcon } from '../../../components/TypeIcon';
+import { constants } from '../../../constants';
+import { captalize } from '../../../utils/captalize';
+
+interface Props {
+  pokemon: Pokemon
+}
+
+export function PokemonItem({ pokemon }: Props) {
+  const formattedID = `# ${pokemon.id.toString().padStart(3, '0')}`
+  const formattedName = captalize(pokemon.name)
+  return (
+    <View style={{...styles.container, backgroundColor: constants.pokemonTypes[pokemon.types[0]].background }}>
+      <View>
+        <Text style={styles.id}>{formattedID}</Text>
+        <Text style={styles.name}>{formattedName}</Text>
+        <View style={styles.types}>
+          {pokemon.types.map(type => (
+            <View
+              key={type}
+              style={{ ...styles.type, backgroundColor: constants.pokemonTypes[type].color }}
+            >
+              <TypeIcon name={type} color='#fff' />
+              <Text style={styles.typeName}>{type}</Text>
+            </View>
+          ))}
+        </View>
+      </View>
+      <Image style={styles.sprite} height={130} width={130} source={{uri: pokemon.sprite}} />
+    </View>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 10,
+    paddingTop: 20,
+    paddingBottom: 20,
+    paddingRight: 10,
+    paddingLeft: 20,
+    marginBottom: 20,
+    height: 115
+  },
+  id: {
+    fontSize: 12,
+    color: '#17171B99'
+  },
+  name: {
+    fontWeight: 'bold',
+    fontSize: 26,
+    lineHeight: 31.93,
+    marginBottom: 5,
+    color: '#fff'
+  },
+  types: {
+    flexDirection: 'row',
+    gap: 5
+  },
+  type: {
+    padding: 5,
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    borderRadius: 5, 
+    gap: 5
+  },
+  typeName: {
+    color: '#fff',
+    fontSize: 12,
+
+  },
+  sprite: {
+    marginTop: -25
+  }
+})
